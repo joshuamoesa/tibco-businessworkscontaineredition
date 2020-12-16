65f810bbe964afa235f72df46d32fce6b079d48b
joshua.moesa@postnl.nl <joshua.moesa@postnl.nl>
2020-12-16 15:55:44 +0100

    Commit locally working Publisher application.

---
+++ AppDynamicsDemo/docker/publisher/0_BUILD_IMAGE AWS.sh
@@ -0,0 +1,2 @@
+#!/bin/sh 
+docker build -f Dockerfile -t 672356061591.dkr.ecr.eu-west-1.amazonaws.com/bwce-fargate:tests3 .
---
+++ AppDynamicsDemo/docker/publisher/1_BUILD_IMAGE LOCAL Base.sh
@@ -0,0 +1,3 @@
+#!/bin/sh 
+cd /Users/joshuamoesa/Applications/tibco/bwce/bwce/2.5/docker
+docker build -t tibco/bwce:base .
